c3a0d9e79bb45dd701bd26a4be0d61c508d60f80
Matthew Ragan <raganmd@gmail.com>
2022-07-08 08:43:27 -0700

    Adjustments from Recordings

--- TouchDesigner-Fundamentals/dev/100/_startDev.cmd
+++ TouchDesigner-Fundamentals/dev/100/_startDev.cmd
@@ -2,7 +2,7 @@
 rem turn off echo
 
 rem set TouchDesigner build numbers
-set TOUCHVERSION=2022.24200
+set TOUCHVERSION=2022.25370
 
 rem set our project file target
 set TOEFILE="TouchDesigner-100-fundamentals.toe"
